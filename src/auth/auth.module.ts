1ea0218fc80337748cdac388801706bdf79c2504
Mahdi <140658028+Mahditgo@users.noreply.github.com>
2025-08-29 20:54:47 +0330

    authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService],
+  imports : [
+    DatabaseModule,
+    JwtModule.register({
+      global : true,
+      secret : process.env.JWT_SECRET,
+      signOptions : { expiresIn : '1h'}
+    })
+  ],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { DatabaseService } from 'src/database/database.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly databaseSercie : DatabaseService,
+        private readonly jwtService : JwtService
+    ) {}
+
+    async register(email : string, password : string, name ?: string) {
+        const hashedePasseword = await bcrypt.hash(password, 10);
+        const user = await this.databaseSercie.user.create({
+            data : {
+                email,
+                password : hashedePasseword,
+                name
+            }
+        });
+
+        return user
+    }
+
+    async login (email : string, password : string) {
+        const user = await this.databaseSercie.user.findUnique({ where: { email } });
+        if (!user) throw new UnauthorizedException('Invalid credentials');
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) throw new UnauthorizedException('Invalid credentials');
+
+        const payload = { sub: user.id, email: user.email };
+        const token = await this.jwtService.signAsync(payload);
+
+        return { access_token: token };
+    }
+
+    async validateUser(userId: string) {
+    return this.databaseSercie.user.findUnique({ where: { id: userId } });
+  }
+}
